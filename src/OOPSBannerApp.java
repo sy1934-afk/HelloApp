148a227eb0a144b15b9a7b8c1726200178f742cc
sy1934-afk <sy1934@srmist.edu.in>
2026-02-16 21:54:29 +0530

    Coded UC2 to Render OOPS as 7-line Banner

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,46 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display
+ *
+ * Displays OOPS in 7-line ASCII Banner Format
+ *
+ * @author Developer
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String O1_line1 = "  *****  ";
+        String O1_line2 = " *     * ";
+        String O1_line3 = " *     * ";
+        String O1_line4 = " *     * ";
+        String O1_line5 = " *     * ";
+        String O1_line6 = " *     * ";
+        String O1_line7 = "  *****  ";
+
+        String P_line1 = " ******* ";
+        String P_line2 = " *     * ";
+        String P_line3 = " *     * ";
+        String P_line4 = " ******* ";
+        String P_line5 = " *       ";
+        String P_line6 = " *       ";
+        String P_line7 = " *       ";
+
+        String S_line1 = "  *****  ";
+        String S_line2 = " *     * ";
+        String S_line3 = " *       ";
+        String S_line4 = "  *****  ";
+        String S_line5 = "       * ";
+        String S_line6 = " *     * ";
+        String S_line7 = "  *****  ";
+
+        System.out.println(O1_line1 + O1_line1 + P_line1 + S_line1);
+        System.out.println(O1_line2 + O1_line2 + P_line2 + S_line2);
+        System.out.println(O1_line3 + O1_line3 + P_line3 + S_line3);
+        System.out.println(O1_line4 + O1_line4 + P_line4 + S_line4);
+        System.out.println(O1_line5 + O1_line5 + P_line5 + S_line5);
+        System.out.println(O1_line6 + O1_line6 + P_line6 + S_line6);
+        System.out.println(O1_line7 + O1_line7 + P_line7 + S_line7);
+
+    }
+}
