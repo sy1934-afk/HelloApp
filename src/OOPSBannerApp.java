63ec50fc9323607dd68a7eee77d76673ca273665
sy1934-afk <sy1934@srmist.edu.in>
2026-02-20 13:46:58 +0530

    UC5: Inline array initialization for OOPS banner

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,64 +1,55 @@
 /**
  * OOPSBannerApp
  *
- * UC4: Render "OOPS" banner using String Array and Loop
+ * UC5: Render "OOPS" banner using inline array initialization
  *
  * @author YourName
- * @version 1.3
+ * @version 1.4
  */
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        // Step 1: Create String array for banner lines
-        String[] bannerLines = new String[7];
+                String[] bannerLines = {
+                                String.join("",
+                                                "  *****  ",
+                                                "  *****  ",
+                                                "  ****** ",
+                                                "  *****  "),
+                                String.join("",
+                                                " **     **",
+                                                " **     **",
+                                                " **    ** ",
+                                                " **       "),
+                                String.join("",
+                                                " **     **",
+                                                " **     **",
+                                                " **    ** ",
+                                                " **       "),
+                                String.join("",
+                                                " **     **",
+                                                " **     **",
+                                                " ******   ",
+                                                "  *****   "),
+                                String.join("",
+                                                " **     **",
+                                                " **     **",
+                                                " **       ",
+                                                "        **"),
+                                String.join("",
+                                                " **     **",
+                                                " **     **",
+                                                " **       ",
+                                                "        **"),
+                                String.join("",
+                                                "  *****  ",
+                                                "  *****  ",
+                                                " **       ",
+                                                "  ******  ")
+                };
 
-        // Step 2: Populate array using String.join()
-        bannerLines[0] = String.join("",
-                "  *****  ",
-                "  *****  ",
-                "  ****** ",
-                "  *****  ");
-
-        bannerLines[1] = String.join("",
-                " **     **",
-                " **     **",
-                " **    ** ",
-                " **       ");
-
-        bannerLines[2] = String.join("",
-                " **     **",
-                " **     **",
-                " **    ** ",
-                " **       ");
-
-        bannerLines[3] = String.join("",
-                " **     **",
-                " **     **",
-                " ******   ",
-                "  *****   ");
-
-        bannerLines[4] = String.join("",
-                " **     **",
-                " **     **",
-                " **       ",
-                "        **");
-
-        bannerLines[5] = String.join("",
-                " **     **",
-                " **     **",
-                " **       ",
-                "        **");
-
-        bannerLines[6] = String.join("",
-                "  *****  ",
-                "  *****  ",
-                " **       ",
-                "  ******  ");
-
-        // Step 3: Loop and print
-        for (String line : bannerLines) {
-            System.out.println(line);
+                for (String line : bannerLines) {
+                        System.out.println(line);
+                }
         }
-    }
 }
